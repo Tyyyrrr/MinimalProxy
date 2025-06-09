c62e8e52ce3887f1215b627e34f985a034b67338
Tyrr <michalkuglin@gmail.com>
2025-06-09 22:08:07 +0200

    dotnet new console application

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace MinimalProxy
+{
+    class Program
+    {
+        
+        static void Main(string[] _)
+        {
+
+            using var listener = new HttpListener();
+
+            Console.WriteLine($"{listener.DefaultServiceNames.Count} DEFAULT SERVICES: ");
+
+            foreach (var service in listener.DefaultServiceNames)
+                Console.WriteLine(service);
+
+
+            Console.WriteLine($"{listener.Prefixes.Count} PREFIXES: ");
+
+            foreach (var prefix in listener.Prefixes)
+                Console.WriteLine(prefix);
+            
+        }
+
+    }
+}
