ecc39021fbf29914a8a52820e64db6c262659b21
Tyrr <michalkuglin@gmail.com>
2025-06-12 23:17:58 +0200

    doc comments for translator interface

--- TranslationInterface/ITranslator.cs
+++ TranslationInterface/ITranslator.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 public interface ITranslator
 {
+    /// <param name="encoding"><see cref="Encoding"/> of the <see href="input"/></param>
+    /// <returns><see href="true"/> if written to <see href="output"/></returns>
     bool TryTranslateRequest(Stream input, Stream output, Encoding encoding);
 
+    /// <param name="encoding"><see cref="Encoding"/> for the <see href="output"/></param>
+    /// <returns><see href="true"/> if written to <see href="output"/></returns>
     bool TryTranslateResponse(Stream input, Stream output, Encoding encoding);
 }
