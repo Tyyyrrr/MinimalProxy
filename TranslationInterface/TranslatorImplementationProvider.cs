027401eb24e00f1aa866c0b575e98c830c3a16e0
Tyrr <michalkuglin@gmail.com>
2025-06-12 19:16:13 +0200

    added error handling

--- TranslationInterface/TranslatorImplementationProvider.cs
+++ TranslationInterface/TranslatorImplementationProvider.cs
@@ -4,20 +4,43 @@ namespace TranslationInterface
 {
     public static class TranslatorImplementationProvider
     {
+        private static string _lastErrorMessage = "Everything is fine";
+
+        public static string GetLastErrorMessage() => _lastErrorMessage;
+
+
         public static bool TryGetTranslatorInstance(Assembly assembly, ref ITranslator instance)
         {
             var type = assembly.GetTypes().FirstOrDefault(t=>typeof(ITranslator).IsAssignableFrom(t));
 
-            if (type == null) 
+            if (type == null)
+            {
+                _lastErrorMessage = $"Type implementing {nameof(ITranslator)} interface has not been found in the assembly";
                 return false;
+            }
 
-            var inst = Activator.CreateInstance(type);
-
-            if(inst == null)
+            object? inst;
+            try
+            {
+                inst = Activator.CreateInstance(type);
+            }
+            catch(Exception ex)
+            {
+                _lastErrorMessage = $"{ex.GetType()} ({ex.Message})";
                 return false;
+            }
+
+            if (inst == null)
+            {
+                _lastErrorMessage = $"Activator returned null instance of type {type}";
+                return false;
+            }
 
             instance = (ITranslator)inst;
+
             return true;
         }
+
+
     }
 }
