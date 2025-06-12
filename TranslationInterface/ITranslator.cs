58ad7435c996a520e528200ef9faa47bb43cd67c
Tyrr <michalkuglin@gmail.com>
2025-06-12 19:11:26 +0200

    removed redundant parameter

--- TranslationInterface/ITranslator.cs
+++ TranslationInterface/ITranslator.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 public interface ITranslator
 {
-    bool TryTranslateRequest(string httpMethod, Stream input, Stream output, Encoding encoding);
+    bool TryTranslateRequest(Stream input, Stream output, Encoding encoding);
 
-    bool TryTranslateResponse(string httpMethod, Stream input, Stream output, Encoding encoding);
+    bool TryTranslateResponse(Stream input, Stream output, Encoding encoding);
 }
