05c49e4889b766346ca64b307c5a0576eca87fb3
Tyrr <michalkuglin@gmail.com>
2025-06-13 08:05:51 +0200

    example translator implementation library project

---
+++ TranslationExample/Translator.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Text;
+using TranslationInterface;
+
+namespace TranslationExample;
+
+public class Translator : ITranslator
+{
+    public bool TryTranslateRequest(Stream input, Stream output, Encoding encoding)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool TryTranslateResponse(Stream input, Stream output, Encoding encoding)
+    {
+        throw new System.NotImplementedException();
+    }
+}
