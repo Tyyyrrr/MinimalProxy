b80b2754b1a9f57c0cb29b8c103806f7d14ec30c
Tyrr <michalkuglin@gmail.com>
2025-06-13 20:43:01 +0200

    refactor ITranslator to operate on high-level HTTP types

--- TranslationInterface/ITranslator.cs
+++ TranslationInterface/ITranslator.cs
@@ -1,14 +1,19 @@
 ﻿namespace TranslationInterface;
 
-using System.Text;
+using System.Net;
 
+/// <summary>
+/// Defines methods for manipulating HTTP requests between client and server
+/// </summary>
 public interface ITranslator
 {
-    /// <param name="encoding"><see cref="Encoding"/> of the <see href="input"/></param>
-    /// <returns><see href="true"/> if written to <see href="output"/></returns>
-    bool TryTranslateRequest(Stream input, Stream output, Encoding encoding);
+    /// <param name="originalRequest">The request sent by the client</param>
+    /// <param name="translatedRequest">The request to send to the server</param>
+    /// <returns>True if conversion has succeeded.</returns>
+    bool TryTranslateRequest(HttpListenerRequest originalRequest, out HttpRequestMessage translatedRequest);
 
-    /// <param name="encoding"><see cref="Encoding"/> for the <see href="output"/></param>
-    /// <returns><see href="true"/> if written to <see href="output"/></returns>
-    bool TryTranslateResponse(Stream input, Stream output, Encoding encoding);
+    /// <param name="originalResponse">The response sent by the server</param>
+    /// <param name="translatedResponse">The response to send to the client</param>
+    /// <returns>True if conversion has succeeded.</returns>
+    bool TryTranslateResponse(HttpResponseMessage originalResponse, out HttpResponseMessage translatedResponse);
 }
