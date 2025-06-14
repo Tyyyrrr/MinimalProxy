6fe27786b44a5324fe5b222aeaa7097d3c40f463
Tyrr <michalkuglin@gmail.com>
2025-06-14 14:30:03 +0200

    docs(ITranslator): make xml comments more accurate and professional

--- TranslationInterface/ITranslator.cs
+++ TranslationInterface/ITranslator.cs
@@ -3,17 +3,27 @@
 using System.Net;
 
 /// <summary>
-/// Defines methods for manipulating HTTP requests between client and server
+/// Provides translation methods for HTTP messages within a proxy context.
 /// </summary>
+/// <remarks>
+/// Note: This interface does not provide access to <see cref="HttpListenerContext"/>.
+/// When constructing responses, the <see cref="Uri"/> parameter should remain empty.
+/// </remarks>
 public interface ITranslator
 {
-    /// <param name="originalRequest">The request sent by the client</param>
-    /// <param name="translatedRequest">The request to send to the server</param>
-    /// <returns>True if conversion has succeeded.</returns>
+    /// <summary>
+    /// Converts an incoming <see cref="HttpListenerRequest"/> to a <see cref="HttpRequestMessage"/> for the target server.
+    /// </summary>
+    /// <param name="originalRequest">The client’s original HTTP request.</param>
+    /// <param name="translatedRequest">The translated request to send to the upstream server.</param>
+    /// <returns><c>true</c> if the translation succeeded; otherwise, <c>false</c>.</returns>
     bool TryTranslateRequest(HttpListenerRequest originalRequest, out HttpRequestMessage translatedRequest);
 
-    /// <param name="originalResponse">The response sent by the server</param>
-    /// <param name="translatedResponse">The response to send to the client</param>
-    /// <returns>True if conversion has succeeded.</returns>
+    /// <summary>
+    /// Converts a <see cref="HttpResponseMessage"/> from the upstream server to a format suitable for the client.
+    /// </summary>
+    /// <param name="originalResponse">The original HTTP response from the server.</param>
+    /// <param name="translatedResponse">The translated response to return to the client.</param>
+    /// <returns><c>true</c> if the translation succeeded; otherwise, <c>false</c>.</returns>
     bool TryTranslateResponse(HttpResponseMessage originalResponse, out HttpResponseMessage translatedResponse);
 }
